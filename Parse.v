b6fcd5864533ae27bed5c13d0620f0dd2814c806
Joe Barrow <joseph.d.barrow@gmail.com>
2017-11-16 16:12:13 -0500

    Updated types to look more like Haskell implementation

--- Parse.v
+++ Parse.v
@@ -2,42 +2,62 @@
 Require Import Coq.Bool.Bool.
 Require Import Coq.Lists.List.
 Require Import Coq.Strings.String.
+Import ListNotations.
 
-Inductive NonTerminal : Type :=
-  | S : NonTerminal.
+Inductive id : Type :=
+  | Id : string -> id.
 
-Inductive Terminal : Type :=
-  | a : Terminal
-  | b : Terminal.      
+Inductive symbol : Type :=
+  | N : id -> symbol
+  | T : id -> symbol
+  | D : id -> symbol.
 
-Inductive Symbol : Type :=
-  | N : NonTerminal -> Symbol
-  | T : Terminal -> Symbol.
+Definition nonterminal (s : symbol) : bool :=
+  match s with
+  | T _ => false
+  | _ => true
+  end.
+
+Definition terminal (s : symbol) : bool :=
+  match s with
+  | T _ => true
+  | _ => false
+  end.
+
+Definition dummy (s : symbol) : bool :=
+  match s with
+  | D _ => true
+  | _ => false
+  end.
 
 Inductive Rule : Type :=
-  | R : NonTerminal -> list Symbol -> Rule.
+  | R : symbol -> list symbol -> Rule.
 
 Inductive Grammar : Type :=
   | G : list Rule -> Grammar.
 
 (* S -> aSa | a *)
 Definition simple_grammar : Grammar :=
-  G ((R S (T a :: N S :: T a :: nil)) :: (R S (T a :: nil)) :: nil).
+  G (
+      (R (N (Id "S")) (T (Id "a") :: N (Id "S") :: T (Id "a") ::  nil)) 
+        :: (R (N (Id "S")) (T (Id "a") :: nil))
+        :: nil
+    ).
 
 Check simple_grammar.
 
-Definition lhs (r : Rule) : NonTerminal :=
+Definition lhs (r : Rule) : symbol :=
   match r with
   | R x _ => x
   end.
 
-Definition rhs (r : Rule) : list Symbol :=
+Definition rhs (r : Rule) : list symbol :=
   match r with
   | R _ ys => ys
   end.
 
 Inductive Tree : Type :=
-  | Leaf : Terminal -> Tree
-  | Node : NonTerminal -> list Tree -> Tree.
+  | Leaf : symbol -> Tree
+  | Node : symbol -> list Tree -> Tree.
 
-Check Node S (Leaf a :: nil).
+Check Node (N (Id "S")) (Leaf (T (Id "a")) :: nil).
