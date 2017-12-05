325feb31c7ed4ce5ea56170b57b5f21d03297fd6
Joe Barrow <joseph.d.barrow@gmail.com>
2017-12-05 15:15:46 -0500

    Progress on Earley Parser

--- Parse.v
+++ Parse.v
@@ -110,19 +110,69 @@ Qed.
 Inductive item : Type :=
   | I : nat -> symbol -> list symbol -> list symbol -> nat -> list Tree -> item.
 
+Notation "'{' i ',' b '==>' s '*' ss ',' j '}'" := (I i b s ss j []) (at level 60).
+
 Definition trees (i : item) : list Tree :=
   match i with
   | I _ _ _ _ _ t => t
   end.
 
-Definition axioms (g : list Rule) : option item :=
+Definition axioms (g : list Rule) : list item :=
   match start_symbol g with
-  | Some s => Some (I 0 (D "S'") [] [s] 0 [])
-  | None => None
+  | Some s => [I 0 (D "S'") [] [s] 0 []]
+  | None => []
   end.
 
+Compute axioms simple_grammar.
+
+(* TODO: Fix goal *)
 Definition goal (g : list Rule) (s : list id) (i : item) : Prop :=
   match i with
   | I i symbol symbols symbols' k trees => i = 0 /\ dummy symbol /\ symbols' = []
   end.
-                                                                                 
+
+(* The `scan` rule for our Earley parser has the form:
+ * 
+ *   i, A → α • wjβ, j
+ * ----------------------
+ * i, A → αwj • β, j + 1
+ *)
+Definition scan (tokens : list id) (trigger : item) : list item :=
+  match trigger with
+  | I i a alpha [] j ts => []
+  | I i a alpha (symbol :: b) j ts => []
+  end.
+
+(* The `predict` rule for our Earley parser has the form:
+ * 
+ * i, A → α • B
+ * ------------- β, j
+ * j, B → •γ, j
+ *
+ *)
+Definition predict (g : list Rule) (trigger : item) : list item :=
+  match trigger with
+  | I i a alpha [] j ts => []
+  | I i a alpha (symbol :: b) j ts => []
+  end.
+
+(* The `complete` rule for our Earley parser has the form:
+ *
+ * i, A → α • Bβ, k k, B → γ•, j
+ * -------------------------------
+ *       i, A → αB • β, j
+ *)
+Definition complete (trigger : item) (stored : list item) : list item :=
+  match trigger with
+  | I i a alpha ((N x) :: b) k ts => []
+  | _ => []
+  end.
+
+Definition consequences
+           (grammar : list Rule) (tokens : list id)
+           (trigger : item) (stored : list item) : list item :=
+  (scan tokens trigger) ++ (predict grammar trigger) ++ (complete trigger stored).
+
+Definition initial_store (g : list Rule) : (list item * list item) :=
+  ([], axioms g).
+
